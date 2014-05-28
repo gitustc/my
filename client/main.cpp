60c1bc84b211ba70af9e59bc436a730b17180e50
anhong <anhong@debian75>
2014-05-28 00:19:39 -0400

    add main.cpp

---
+++ client/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename: main.cpp
+ *        Created: 05/28/2014 12:19:17 AM
+ *  Last Modified: 05/28/2014 12:19:23 AM
+ *
+ *    Description: 
+ *
+ *        Version: 1.0
+ *       Revision: none
+ *       Compiler: gcc
+ *
+ *         Author: ANHONG
+ *          Email: anhonghe@gmail.com
+ *   Organization: USTC
+ *
+ * =====================================================================================
+ */
+
+
